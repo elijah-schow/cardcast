c9fa80f4373aff4ce940365355b04d98872bc892
Elijah Schow <elijah.schow@gmail.com>
2017-03-25 18:16:20 -0500

    Continue fixing chrome cast view

--- clients/receiver/components/app.js
+++ clients/receiver/components/app.js
@@ -102,7 +102,7 @@ angular.module('cardcast-receiver', [
   return {
     restrict: 'E',
     scope: { content: '=' },
-    template: '/assets/projector.html',
+    templateUrl: '/assets/projector.html',
     link: function($scope, $element){
       const aspectRatio = $window.innerWidth / $window.innerHeight;
       const width = $element.find('div')[0].offsetWidth;
